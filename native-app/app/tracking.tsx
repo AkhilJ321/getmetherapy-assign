f653330a727ad6d8cb8b77f843ae3f4dbe8a9a9e
AkhilJ321 <jamwalakhil936@gmail.com>
2024-07-17 13:06:23 +0530

    add api for quote display

--- native-app/app/tracking.tsx
+++ native-app/app/tracking.tsx
@@ -4,9 +4,27 @@ import { View, ImageBackground, Dimensions, Text } from "react-native";
 import Clock from "@/components/Clock";
 
 const Tracking = () => {
+  const [quote, setQuote] = useState<string | null>("");
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await fetch("https://api.quotable.io/random");
+      const data = await response.json();
+      console.log("Quote", data.content);
+      setQuote(data.content);
+    };
+    fetchQuote();
+
+    const interval = setInterval(fetchQuote, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Clock />
+      <Text className=" text-[18px] mb-8 italic">"{quote}"</Text>
     </View>
   );
 };
